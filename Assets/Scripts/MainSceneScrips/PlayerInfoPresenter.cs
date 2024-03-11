d96b8f17bd43e86725ff1d8ee0812a4223a45689
adendesu <adenhaya031@icloud.com>
2024-03-12 04:50:22 +0900

    Buttle

--- Assets/Scripts/MainSceneScrips/PlayerInfoPresenter.cs
+++ Assets/Scripts/MainSceneScrips/PlayerInfoPresenter.cs
@@ -18,5 +18,6 @@ public class PlayerInfoPresenter : MonoBehaviour
     void DisplayUI()
     {
         playerUI.DisplayPlayerInfo(playerInfoModel.playerName.Value, playerInfoModel.playerRank.Value.ToString(),playerInfoModel.playerExp.Value);
+        Debug.Log(playerInfoModel.playerName.Value);
     }
 }
